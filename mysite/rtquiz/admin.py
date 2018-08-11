97b1d57756428f3b58c29f61b9c3fd16bcc725f1
carlos <carlosdanielcsantos@gmail.com>
2018-08-11 20:07:35 +0100

    Migrate to Quiz/Question/Answers models

--- mysite/rtquiz/admin.py
+++ mysite/rtquiz/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Quiz, Question, Answer
 
+admin.site.register(Quiz)
 admin.site.register(Question)
+admin.site.register(Answer)
+
--- mysite/rtquiz/models.py
+++ mysite/rtquiz/models.py
@@ -4,22 +4,32 @@ from django.utils import timezone
 import datetime
 
 
+class Quiz(models.Model):
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField()
+
+    def was_published_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    answerable = models.BooleanField(default=False)
 
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-
-class Choice(models.Model):
+class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    answer_text = models.TextField()
+    author = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.choice_text
-
+        return "=====\n + " + \
+               "author: " + self.author + \
+               "-----\n" + self.answer_text
