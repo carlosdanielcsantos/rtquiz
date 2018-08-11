c4e075a18a4c7a6ea9b0b190ccc7e530796fd7ec
carlos <carlosdanielcsantos@gmail.com>
2018-08-11 20:06:31 +0100

    Complete tutorial

--- mysite/rtquiz/views.py
+++ mysite/rtquiz/views.py
@@ -1,23 +1,42 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Answer
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list,
                'n_questions': Question.objects.count(),
                'n_latest_questions': len(latest_question_list)}
-    return HttpResponse(render(request, 'rtquiz/index.html', context))
+    return render(request, 'rtquiz/index.html', context)
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'rtquiz/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s." % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'rtquiz/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
 
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redispay the voting form
+        return render(request, 'rtquiz/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('rtquiz:results',
+                                            args=(question.id,)))
